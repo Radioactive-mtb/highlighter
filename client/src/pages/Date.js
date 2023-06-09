0f3512b3e2783e9c614cf20a8f9a01388e53095c
Ana Campos <arcampos171@gmail.com>
2023-06-08 20:16:06 -0700

    readme file created-still need page layout image once styling is completed and deployed link; created logo added to Date.js file

--- client/src/pages/Date.js
+++ client/src/pages/Date.js
@@ -1,26 +1,28 @@
-import  React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import dayjs from 'dayjs';
-import "./Date.css";
-
+import './Date.css';
 
 const DateDisplay = () => {
-    const [date, setDate] = useState(new Date());
-    const day = dayjs(date).format("MM");
-    const formattedDate = dayjs(date).format("MMMM YYYY");
-  
-    useEffect(() => {
-        var changeTime = setInterval(()=>setDate(new Date()), 1000 )
-        return function changeTimer() {
-            clearInterval(changeTime)
-        }
-    })
+  const [date, setDate] = useState(new Date());
+  const day = dayjs(date).format('MM');
+  const formattedDate = dayjs(date).format('MMMM YYYY');
 
-    return (
-      <div className="row date-display p-3 text-center">
-        <div className='col'>{day} | {formattedDate}</div>
+  useEffect(() => {
+    var changeTime = setInterval(() => setDate(new Date()), 1000);
+    return function changeTimer() {
+      clearInterval(changeTime);
+    };
+  }, []);
+
+  return (
+    <div className="row date-display p-3 text-center">
+      <div className="col">
+        {day} | {formattedDate}
+        <img src="/logo.png" alt="Logo" className="logo" style={{ width: '200px', height: '200px' }} />
       </div>
-    );
-  };
-  
-  export default DateDisplay;
-  
+    </div>
+  );
+};
+
+export default DateDisplay;
+
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -17,3 +17,4 @@ const Homepage = () => {
 
 export default Homepage;
 
+
